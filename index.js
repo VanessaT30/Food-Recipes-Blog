88f4a24a3449ea2939d1e66013177662f99214ee
Vanessa Tolentino <VTolentino@gmail.com>
2025-10-20 21:31:25 +0100

    Changed the html files to ejs and moved the css files to the public folder

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+    res.render(__);
 });
 
+app.get("/savoury", (req, res) => {
+    res.render(__dirname + "/views/savoury.ejs");
+});
+
+app.get("/sweet", (req, res) => {
+    res.render(__dirname + "/views/sweet.ejs");
+});
 
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
 });
+
